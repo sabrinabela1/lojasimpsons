6db796ed7d7f6b4afd09f04fa5624b06c43da83c
sabrinabela1 <167808011+sabrinabela1@users.noreply.github.com>
2024-05-20 16:56:40 -0300

    Add files via upload

---
+++ auto.sql
@@ -0,0 +1,16 @@
+#Comando para criar a base de dados
+create database auto;
+
+#Selecione a base de dados
+use auto;
+
+#Criar tabela
+create table carro(
+id int auto_increment not null,
+marca varchar(60) null,
+ano int null,
+primary key(id)
+);
+
+#Selecionar tabela
+select * from carro;
